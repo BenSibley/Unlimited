6597bd6d4152322303ce8cf42a3c4faee038cb23
BenSibley <ben@competethemes.com>
2014-11-18 21:53:38 -0500

    logo now using postMessage

--- inc/customizer.php
+++ inc/customizer.php
@@ -19,6 +19,7 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 		'type'              => 'theme_mod',
 		'capability'        => 'edit_theme_options',
 		'sanitize_callback' => 'esc_url_raw',
+		'transport'         => 'postMessage'
 	) );
 	// control
 	$wp_customize->add_control( new WP_Customize_Image_Control(
--- js/customizer.js
+++ js/customizer.js
@@ -1,3 +1,27 @@
 jQuery(document).ready(function($){
 
+    /*
+     * Following functions are for utilizing the postMessage transport setting
+     */
+
+    wp.customize( 'logo_upload', function( value ) {
+        value.bind( function( newval ) {
+            // get the <a> holding the logo/site title
+            var logoContainer = $('#customize-preview iframe').contents().find('#site-title').find('a');
+            // get the name of the site from the <a>
+            var siteTitle = logoContainer.attr('title');
+            // if there is an image, add the image markup
+            if( newval ) {
+                var logo = "<span class='screen-reader-text'>" + siteTitle + "</span><img id='logo' class='logo' src='" + newval + "' alt='" + siteTitle + "' />";
+            }
+            // otherwise just use the site title
+            else {
+                var logo = siteTitle;
+            }
+            // empty the content first
+            logoContainer.empty();
+            // replace with the new logo markup
+            logoContainer.append(logo);
+        } );
+    } );
 });
