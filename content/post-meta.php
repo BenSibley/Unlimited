2b4aef59ff1a9e25a3cb9d40e1a5d012f7ba50a8
BenSibley <ben@competethemes.com>
2014-11-21 23:16:12 -0500

    more style updates

--- content/post-meta.php
+++ content/post-meta.php
@@ -13,7 +13,9 @@
 		<i class="fa fa-comment"></i>
 		<?php
 		if( ! comments_open() && get_comments_number() < 1 ) :
-			comments_number( __( 'Comments closed', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
+			echo '<span>';
+				comments_number( __( 'Comments closed', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
+			echo '</span>';
 		else :
 			echo '<a href="' . get_comments_link() . '">';
 				comments_number( __( 'Leave a Comment', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
