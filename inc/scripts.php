bdf711e7c6319a6c6cb76b8d06cd1071d87601e8
BenSibley <ben@competethemes.com>
2014-11-25 10:17:07 -0500

    admin stylesheet now loading on theme options page

--- inc/scripts.php
+++ inc/scripts.php
@@ -43,6 +43,12 @@ function ct_unlimited_enqueue_admin_styles($hook){
 		// enqueue the JS needed to utilize media uploader on profile image upload
 		wp_enqueue_script('ct-unlimited-profile-image-uploader', get_template_directory_uri() . '/js/build/profile-image-uploader.min.js');
 	}
+	// if theme options page
+	if( 'appearance_page_unlimited-options' == $hook ) {
+
+		// Admin styles
+		wp_enqueue_style('ct-unlimited-admin-styles', get_template_directory_uri() . '/styles/admin.min.css');
+	}
 }
 add_action('admin_enqueue_scripts',	'ct_unlimited_enqueue_admin_styles' );
 
