82032210a98ee6fb8956a0cc621871dfa4c59c00
BenSibley <ben@competethemes.com>
2014-11-25 10:22:06 -0500

    theme options page setup

--- theme-options.php
+++ theme-options.php
@@ -13,21 +13,21 @@ function ct_unlimited_options_content(){
         <h2><?php _e('unlimited Dashboard', 'unlimited'); ?></h2>
         <div class="content content-customization">
             <h3><?php _e('Customization', 'unlimited'); ?></h3>
-            <p><?php _e('Click the "Customize" link in your menu, or use the button below to get started customizing unlimited', 'unlimited'); ?>.</p>
+            <p><?php _e('Click the "Customize" link in your menu, or use the button below to get started customizing Unlimited', 'unlimited'); ?>.</p>
             <p>
                 <a class="button-primary" href="customize.php"><?php _e('Use Customizer', 'unlimited') ?></a>
             </p>
         </div>
         <div class="content content-support">
 	        <h3><?php _e('Support', 'unlimited'); ?></h3>
-            <p><?php _e("You can find the knowledgebase, changelog, support forum, and more in the unlimited Support Center", "unlimited"); ?>.</p>
+            <p><?php _e("You can find the knowledgebase, changelog, support forum, and more in the Unlimited Support Center", "unlimited"); ?>.</p>
             <p>
-                <a target="_blank" class="button-primary" href="http://www.competethemes.com/documentation/unlimited-support-center/"><?php _e('Visit Support Center', 'unlimited'); ?></a>
+                <a target="_blank" class="button-primary" href="https://www.competethemes.com/documentation/unlimited-support-center/"><?php _e('Visit Support Center', 'unlimited'); ?></a>
             </p>
         </div>
         <div class="content content-premium-upgrade">
-            <h3><?php _e('Upgrade to unlimited Plus ($29)', 'unlimited'); ?></h3>
-            <p><?php _e('unlimited Plus is the premium version of unlimited. By upgrading to unlimited Plus, you get:', 'unlimited'); ?></p>
+            <h3><?php _e('Unlimited Pro ($49)', 'unlimited'); ?></h3>
+            <p><?php _e('Unlimited Pro is the premium version of Unlimited. By upgrading, you get:', 'unlimited'); ?></p>
             <ul>
                 <li><?php _e('Custom colors', 'unlimited'); ?></li>
                 <li><?php _e('Background images & textures', 'unlimited'); ?></li>
@@ -35,7 +35,7 @@ function ct_unlimited_options_content(){
                 <li><?php _e('and much more&#8230;', 'unlimited'); ?></li>
             </ul>
             <p>
-                <a target="_blank" class="button-primary" href="https://www.competethemes.com/unlimited-plus/"><?php _e('See Full Feature List', 'unlimited'); ?></a>
+                <a target="_blank" class="button-primary" href="https://www.competethemes.com/unlimited-pro/"><?php _e('See Full Feature List', 'unlimited'); ?></a>
             </p>
         </div>
     </div>
