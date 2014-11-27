037118dc043cb493d3e414424134ec7230d8ce13
BenSibley <ben@competethemes.com>
2014-11-26 22:19:18 -0500

    full-width template rebuilt and now working

--- templates/full-width.php
+++ templates/full-width.php
@@ -2,24 +2,15 @@
 /**
  * Template Name: Full-width
  */
-?>
 
-<?php get_header(); ?>
+get_header();
 
-<div <?php post_class(); ?>>
-	<article>
-		<?php ct_unlimited_featured_image(); ?>
-		<div class="post-padding-container">
-			<div class='post-header'>
-				<h1 class='post-title'><?php the_title(); ?></h1>
-			</div>
-			<div class="post-content">
-				<?php the_content(); ?>
-				<?php wp_link_pages(array('before' => '<p class="singular-pagination">' . __('Pages:','unlimited'), 'after' => '</p>', ) ); ?>
-			</div>
-			<?php comments_template(); ?>
-		</div>
-	</article>
-</div>
+// The loop
+if ( have_posts() ) :
+	while (have_posts() ) :
+		the_post();
+		get_template_part( 'content', 'page' );
+	endwhile;
+endif;
 
-<?php get_footer(); ?>
+get_footer();
