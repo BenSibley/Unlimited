f8a3877ef5a46f1ec2a4d87dfd0e788eaea8988b
BenSibley <ben@competethemes.com>
2014-11-28 13:54:58 -0500

    commented main JS file

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,63 @@
 jQuery(document).ready(function($){
 
+    // add fitvids to all vids in posts/pages
     $('.post').fitVids();
 
+    // open the search bar in site-header on click
     $('#search-icon').on('click', openSearchBar);
 
     function openSearchBar(){
 
+        // get the social icons
         var socialIcons = $('#site-header').find('.social-media-icons');
 
+        // if search bar already open
         if( $(this).hasClass('open') ) {
+
+            // remove styling class
             $(this).removeClass('open');
+
+            // remove styling class
             if( socialIcons.hasClass('fade') ) {
                 socialIcons.removeClass('fade');
             }
+
+            // make search input inaccessible to keyboards
             $('#site-header').find('.search-field').attr('tabindex', -1);
+
         } else {
+
+            // add styling class
             $(this).addClass('open');
+
+            // if search input is still 100%, add styling class
             if( $(window).width() < 600 ) {
                 socialIcons.addClass('fade');
             }
+
+            // make search input keyboard accessible
             $('#site-header').find('.search-field').attr('tabindex', 0);
         }
     }
 
+    // display the primary menu at mobile widths
     $('#toggle-navigation').on('click', openPrimaryMenu);
 
     function openPrimaryMenu() {
 
+        // get height of the menu
         var menuHeight = calculateMenuHeight();
 
+        // if menu open
         if( $(this).hasClass('open') ) {
+
+            // remove styling class
             $(this).removeClass('open');
-            // close menu and all submenus
+
+            // close all ULs by removing increased max-height
             $('#menu-primary, #menu-primary-items ul, .menu-unset ul').removeAttr('style');
+
+            // close all ULs and require 2 clicks again when reopened
             $('.menu-item-has-children').each(function(){
                 if( $(this).hasClass('open') ) {
                     $(this).removeClass('open');
@@ -40,11 +65,16 @@ jQuery(document).ready(function($){
                 }
             });
         } else {
+
+            // add styling class to reveal primary menu
             $(this).addClass('open');
+
             // open to show whole menu plus 48px of padding for style
             $('#menu-primary').css('max-height', menuHeight + 48);
         }
     }
+
+    // get height of primary menu
     function calculateMenuHeight() {
 
         if( $('#menu-primary-items').length > 0 ) {
@@ -54,14 +84,15 @@ jQuery(document).ready(function($){
         }
         return menuHeight;
     }
+
     // enable double-click menu parent items right away
     if( $(window).width() < 800 ) {
         enableTouchDropdown();
     } else {
-        // wait to see if a touch event is fired
-        var hasTouch;
+        // otherwise wait to see if a touch event is fired
         $(window).on('touchstart', enableTouchDropdown, false );
     }
+
     // add the double-click if the menu is made smaller again
     $(window).resize(function(){
         if( $(window).width() < 800 ) {
@@ -83,7 +114,7 @@ jQuery(document).ready(function($){
         menuParents.on('click', openDropdown);
     }
 
-    // open the dropdown without visiting parent link
+    // open the dropdown without visiting parent link on first click
     function openDropdown(e){
 
         // don't enforce if screen resized over 800px
@@ -91,20 +122,27 @@ jQuery(document).ready(function($){
 
             // if the menu item is not showing children
             if ($(this).hasClass('closed')) {
+
                 // prevent link from being visited
                 e.preventDefault();
+
                 // add an open class
                 $(this).addClass('open');
+
                 // get the submenu
                 var submenu = $(this).children('ul');
+
                 // set variable
                 var submenuHeight = 0;
+
                 // get height of all menu items in submenu combined
                 submenu.children('li').each(function () {
                     submenuHeight = submenuHeight + $(this).height();
                 });
+
                 // set new max-height to open submenu
                 submenu.css('max-height', submenuHeight);
+
                 // remove 'closed' class to enable link
                 $(this).removeClass('closed');
 
@@ -140,6 +178,7 @@ jQuery(document).ready(function($){
     $('.menu-item a, .page_item a').focusout(function(){
         $(this).parents('ul').removeClass('focused');
     });
+
 });
 
 /* fix for skip-to-content link bug in Chrome & IE9 */
