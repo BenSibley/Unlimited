90940a9095bbf1dae5137537d942acdcce8eac23
BenSibley <ben@competethemes.com>
2014-12-23 11:16:32 -0500

    switched add_editor_style to admin_init

--- inc/scripts.php
+++ inc/scripts.php
@@ -94,4 +94,4 @@ add_filter('clean_url', 'ct_unlimited_add_async_script', 11, 1);
 function ct_unlimited_add_editor_styles() {
 	add_editor_style( 'styles/custom-editor-style.min.css' );
 }
-add_action( 'init', 'ct_unlimited_add_editor_styles' );
+add_action( 'admin_init', 'ct_unlimited_add_editor_styles' );
