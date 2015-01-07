62ca0d8b829f7e241876291487b1988de18862a8
BenSibley <ben@competethemes.com>
2015-01-07 10:22:56 -0500

    updated post meta markup so link contains icon

--- content/post-meta.php
+++ content/post-meta.php
@@ -1,24 +1,28 @@
 <div id="post-meta" class="post-meta">
 	<div class="date-meta">
-		<i class="fa fa-calendar"></i>
 		<a href="<?php echo get_month_link( get_the_date('Y'), get_the_date('n') ); ?>" title="Posts from <?php echo get_the_date('F'); ?>">
-			<?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'n/j/Y' ) ) ); ?>
+			<i class="fa fa-calendar"></i>
+			<span><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'n/j/Y' ) ) ); ?></span>
 		</a>
 	</div>
 	<div class="author-meta">
-		<i class="fa fa-user"></i>
-		<?php the_author_posts_link(); ?>
+		<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="Posts by <?php the_author(); ?>">
+			<i class="fa fa-user"></i>
+			<span><?php the_author(); ?></span>
+		</a>
 	</div>
 	<div class="comments-meta">
-		<i class="fa fa-comment"></i>
 		<a href="<?php echo get_comments_link(); ?>" title="Comments for this post">
-			<?php
-			if( ! comments_open() && get_comments_number() < 1 ) :
-				comments_number( __( 'Comments closed', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
-			else :
-				comments_number( __( 'Leave a Comment', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
-			endif;
-			?>
+			<i class="fa fa-comment"></i>
+			<span>
+				<?php
+				if( ! comments_open() && get_comments_number() < 1 ) :
+					comments_number( __( 'Comments closed', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
+				else :
+					comments_number( __( 'Leave a Comment', 'unlimited' ), __( 'One Comment', 'unlimited'), __( '% Comments', 'unlimited' ) );
+				endif;
+				?>
+			</span>
 		</a>
 	</div>
 </div>
