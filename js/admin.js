d61c6b422aef848081db2ca9044431b1e198bef0
BenSibley <ben@competethemes.com>
2015-07-02 14:51:48 -0400

    ajax functionality added and admin.min.js file created

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,15 @@
-/**
- * Created by bensibley on 7/2/15.
- */
+jQuery(document).ready(function($) {
+
+    $('#unlimited-avatar-notice').find('.notice-dismiss').bind('click', function(){
+
+        // set up data object
+        var data = {
+            action: 'dismiss_unlimited_avatar_notice',
+            dismissed: true,
+            security: '<?php echo $ajax_nonce; ?>'
+        };
+
+        // post data received from PHP respond
+        jQuery.post(ajaxurl, data);
+    });
+});
