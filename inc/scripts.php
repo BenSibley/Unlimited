e3ffdbe0ed0ab3c8003a05ea40a40ec304e47e78
BenSibley <ben@competethemes.com>
2015-07-08 09:18:41 -0400

    stopped loading primary js file asynchronously

--- inc/scripts.php
+++ inc/scripts.php
@@ -6,7 +6,7 @@
 function unlimited_load_scripts_styles() {
 
 	// main JS file
-	wp_enqueue_script('ct-unlimited-js', get_template_directory_uri() . '/js/build/production.min.js#unlimited_asyncload', array('jquery'),'', true);
+	wp_enqueue_script('ct-unlimited-js', get_template_directory_uri() . '/js/build/production.min.js', array('jquery'),'', true);
 	wp_localize_script( 'ct-unlimited-js', 'objectL10n', array(
 		'openMenu'  => __( 'open menu', 'unlimited' ),
 		'closeMenu' => __( 'close menu', 'unlimited' )
