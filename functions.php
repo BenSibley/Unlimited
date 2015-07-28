6b3d40a6273c9ec35a608a84bbc2df35ec6465a0
BenSibley <ben@competethemes.com>
2015-07-28 11:21:53 -0400

    removed profile image function and all uses of it, and removed the image id retrieval function

--- functions.php
+++ functions.php
@@ -83,12 +83,7 @@ if( ! function_exists( 'unlimited_customize_comments' ) ) {
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
 			<div class="comment-author">
 				<?php
-				// if is post author
-				if ( $comment->user_id === $post->post_author ) {
-					unlimited_profile_image_output();
-				} else {
 					echo get_avatar( get_comment_author_email(), 48, '', get_comment_author() );
-				}
 				?>
 				<div class="author-name">
 					<span><?php comment_author_link(); ?></span> <?php _x( 'said:', 'unlimited', 'the commenter said the following:' ); ?>
@@ -428,50 +423,6 @@ if( ! function_exists('unlimited_social_icons_output') ) {
 			echo "</ul>";
 		}
 	}
-}
-
-// retrieves the attachment ID from the file URL
-function unlimited_get_image_id($url) {
-
-    // Split the $url into two parts with the wp-content directory as the separator
-    $parsed_url  = explode( parse_url( WP_CONTENT_URL, PHP_URL_PATH ), $url );
-
-    // Get the host of the current site and the host of the $url, ignoring www
-    $this_host = str_ireplace( 'www.', '', parse_url( home_url(), PHP_URL_HOST ) );
-    $file_host = str_ireplace( 'www.', '', parse_url( $url, PHP_URL_HOST ) );
-
-    // Return nothing if there aren't any $url parts or if the current host and $url host do not match
-    if ( ! isset( $parsed_url[1] ) || empty( $parsed_url[1] ) || ( $this_host != $file_host ) ) {
-        return;
-    }
-
-    // Now we're going to quickly search the DB for any attachment GUID with a partial path match
-    // Example: /uploads/2013/05/test-image.jpg
-    global $wpdb;
-
-    $attachment = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM {$wpdb->prefix}posts WHERE guid RLIKE %s;", $parsed_url[1] ) );
-
-    // Returns null if no attachment is found
-    return $attachment[0];
-}
-
-function unlimited_profile_image_output(){
-
-    // use User's profile image, else default to their Gravatar
-    if(get_the_author_meta('unlimited_user_profile_image')){
-
-        // get the id based on the image's URL
-        $image_id = unlimited_get_image_id(get_the_author_meta('unlimited_user_profile_image'));
-
-        // retrieve the thumbnail size of profile image (60px)
-        $image_thumb = wp_get_attachment_image($image_id, array(60,60), false, array('alt' => get_the_author() ) );
-
-        // display the image
-        echo $image_thumb;
-
-    } else {
-        echo get_avatar( get_the_author_meta( 'ID' ), 60, '', get_the_author() );
-    }
 }
 
 function unlimited_wp_backwards_compatibility() {
