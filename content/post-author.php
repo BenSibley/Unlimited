e8d30c8310bd3cb1ee4d95671e908c80471fe0c9
BenSibley <ben@competethemes.com>
2015-12-17 12:06:56 -0500

    post tag markup no longer output if tags not present

--- content/post-author.php
+++ content/post-author.php
@@ -1,7 +1,11 @@
 <div class="post-author">
 	<?php echo get_avatar( get_the_author_meta( 'ID' ), 60, '', get_the_author() ); ?>
-	<h3><?php echo get_the_author(); ?></h3>
+	<h3>
+		<?php echo get_the_author(); ?>
+	</h3>
 	<?php unlimited_social_icons_output('author') ?>
-	<p><?php the_author_meta('description'); ?></p>
+	<p>
+		<?php the_author_meta('description'); ?>
+	</p>
 	<a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>"><?php _e('View more posts', 'unlimited'); ?></a>
 </div>
--- content/post-tags.php
+++ content/post-tags.php
@@ -1,15 +1,11 @@
 <?php
-$tags = get_the_tags( $post->ID );
-$output = '';
-if($tags){
-	foreach($tags as $tag) {
-		$output .= '<li><a href="'.get_tag_link( $tag->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts tagged %s", 'unlimited' ), $tag->name ) ) . '">'.$tag->name.'</a></li>';
-	}
-} 
-?>
 
-<div class="post-tags">
-	<ul>
-		<?php echo $output; ?>
-	</ul>
-</div> 
+$tags = get_the_tags( $post->ID );
+
+if( $tags ){
+	echo '<div class="post-tags"><ul>';
+	foreach($tags as $tag) {
+		echo '<li><a href="'.get_tag_link( $tag->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts tagged %s", 'unlimited' ), $tag->name ) ) . '">'.$tag->name.'</a></li>';
+	}
+	echo '</ul></div>';
+}
