abf930e254741e74ffb935e4178ccb6919167b6d
BenSibley <ben@competethemes.com>
2015-12-16 19:34:10 -0500

    index if/else statements moved into content function

--- index.php
+++ index.php
@@ -3,40 +3,14 @@
 get_template_part('content/archive-header'); ?>
 
 <div id="loop-container" class="loop-container">
-
-<?php
-// The loop
-if ( have_posts() ) :
-    while (have_posts() ) :
-        the_post();
-
-        /* Blog */
-        if( is_home() ) {
-            get_template_part( 'content', 'archive' );
-        }
-        /* Post */
-        elseif( is_singular( 'post' ) ) {
-            get_template_part( 'content' );
-        }
-        /* Page */
-        elseif( is_page() ) {
-            get_template_part( 'content', 'page' );
-        }
-        /* Attachment */
-        elseif( is_attachment() ) {
-            get_template_part( 'content', 'attachment' );
-        }
-        /* Archive */
-        elseif( is_archive() ) {
-            get_template_part( 'content', 'archive' );
-        }
-        /* Custom Post Type */
-        else {
-            get_template_part( 'content' );
-        }
-    endwhile;
-endif; ?>
-
+    <?php
+    if ( have_posts() ) :
+        while (have_posts() ) :
+            the_post();
+            unlimited_get_content_template();
+        endwhile;
+    endif;
+    ?>
 </div>
 
 <?php echo unlimited_loop_pagination(); ?>
