171f9cc34e6ac114b495bb69c44ccc4cfad038bc
BenSibley <ben@competethemes.com>
2016-02-08 08:16:43 -0500

    minor copy change

--- theme-options.php
+++ theme-options.php
@@ -34,8 +34,16 @@ function unlimited_options_content() {
 				   href="https://www.competethemes.com/documentation/unlimited-support-center/"><?php _e( 'Visit Support Center', 'unlimited' ); ?></a>
 			</p>
 		</div>
+		<div class="content content-support">
+			<h3><?php _e( 'Support', 'unlimited' ); ?></h3>
+			<p><?php _e( "You can find the knowledgebase, changelog, support forum, and more in the Unlimited Support Center.", "unlimited" ); ?></p>
+			<p>
+				<a target="_blank" class="button-primary"
+				   href="https://www.competethemes.com/documentation/unlimited-support-center/"><?php _e( 'Visit Support Center', 'unlimited' ); ?></a>
+			</p>
+		</div>
 		<div class="content content-premium-upgrade">
-			<h3><?php _e( 'Get More Features & Flexibility', 'unlimited' ); ?></h3>
+			<h3><?php _e( 'Unlimited Pro Plugin', 'unlimited' ); ?></h3>
 			<p><?php _e( 'Download the Unlimited Pro plugin and unlock custom colors, new layouts, background images, and more...', 'unlimited' ); ?></p>
 			<p>
 				<a target="_blank" class="button-primary"
