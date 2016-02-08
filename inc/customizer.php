3e75a9a6940b5467a9b2d71b6a50e88f9bb0dcd8
BenSibley <ben@competethemes.com>
2016-02-08 08:24:32 -0500

    missing period

--- inc/customizer.php
+++ inc/customizer.php
@@ -196,7 +196,7 @@ function unlimited_add_customizer_content( $wp_customize ) {
 		'transport'         => 'postMessage'
 	) );
 
-	$description_layout = sprintf( __( 'Want more layouts? <a target="_blank" href="%s">Check out Unlimited Pro</a>', 'unlimited' ), 'https://www.competethemes.com/unlimited-pro/' );
+	$description_layout = sprintf( __( 'Want more layouts? <a target="_blank" href="%s">Check out Unlimited Pro</a>.', 'unlimited' ), 'https://www.competethemes.com/unlimited-pro/' );
 
 	// control
 	$wp_customize->add_control( 'layout', array(
