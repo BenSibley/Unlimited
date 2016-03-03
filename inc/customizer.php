c3cadfd6df0545d1a71898cd79e9d9a1e7bb3b5b
BenSibley <ben@competethemes.com>
2016-03-03 11:14:43 -0500

    added reference to skype link protocol

--- inc/customizer.php
+++ inc/customizer.php
@@ -137,21 +137,28 @@ function unlimited_add_customizer_content( $wp_customize ) {
 					'sanitize_callback' => 'ct_unlimited_sanitize_skype',
 					'transport'         => 'postMessage'
 				) );
+				// control
+				$wp_customize->add_control( $social_site, array(
+					'type'        => 'url',
+					'label'       => $label,
+					'description' => sprintf( __( 'Accepts Skype link protocol (<a href="%s" target="_blank">learn more</a>)', 'unlimited' ), 'https://www.competethemes.com/blog/skype-links-wordpress/' ),
+					'section'     => 'unlimited_social_media_icons',
+					'priority'    => $priority
+				) );
 			} else {
 				// setting
 				$wp_customize->add_setting( $social_site, array(
 					'sanitize_callback' => 'esc_url_raw',
 					'transport'         => 'postMessage'
 				) );
+				// control
+				$wp_customize->add_control( $social_site, array(
+					'type'     => 'url',
+					'label'    => $label,
+					'section'  => 'unlimited_social_media_icons',
+					'priority' => $priority
+				) );
 			}
-
-			// control
-			$wp_customize->add_control( $social_site, array(
-				'type'     => 'url',
-				'label'    => $label,
-				'section'  => 'unlimited_social_media_icons',
-				'priority' => $priority
-			) );
 		}
 		// increment the priority for next site
 		$priority = $priority + 5;
