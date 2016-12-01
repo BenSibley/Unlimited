fc3225effab5d38db26e684f4b7ed7f4dc53a79b
BenSibley <ben@competethemes.com>
2016-12-01 12:41:48 -0500

    updated dashboard

--- theme-options.php
+++ theme-options.php
@@ -18,20 +18,12 @@ function unlimited_options_content() {
 	<div id="unlimited-dashboard-wrap" class="wrap">
 		<h2><?php _e( 'Unlimited Dashboard', 'unlimited' ); ?></h2>
 		<?php do_action( 'theme_options_before' ); ?>
-		<div class="content content-customization">
-			<h3><?php _e( 'Customization', 'unlimited' ); ?></h3>
-			<p><?php _e( 'Click the "Customize" link in your menu, or use the button below to get started customizing Unlimited.', 'unlimited' ); ?></p>
-			<p>
-				<a class="button-primary"
-				   href="<?php echo esc_url( $customizer_url ); ?>"><?php _e( 'Use Customizer', 'unlimited' ) ?></a>
-			</p>
-		</div>
 		<div class="content content-support">
-			<h3><?php _e( 'Support', 'unlimited' ); ?></h3>
-			<p><?php _e( "You can find the knowledgebase, changelog, support forum, and more in the Unlimited Support Center.", "unlimited" ); ?></p>
+			<h3><?php _e( 'Get Started', 'unlimited' ); ?></h3>
+			<p><?php _e( "Not sure where to start? The <strong>Unlimited Getting Started Guide</strong> will take you step-by-step through every feature in Unlimited.", "unlimited" ); ?></p>
 			<p>
 				<a target="_blank" class="button-primary"
-				   href="https://www.competethemes.com/documentation/unlimited-support-center/"><?php _e( 'Visit Support Center', 'unlimited' ); ?></a>
+				   href="https://www.competethemes.com/help/getting-started-unlimited/"><?php _e( 'View Guide', 'unlimited' ); ?></a>
 			</p>
 		</div>
 		<div class="content content-premium-upgrade">
@@ -40,14 +32,6 @@ function unlimited_options_content() {
 			<p>
 				<a target="_blank" class="button-primary"
 				   href="https://www.competethemes.com/unlimited-pro/"><?php _e( 'See Full Feature List', 'unlimited' ); ?></a>
-			</p>
-		</div>
-		<div class="content content-resources">
-			<h3><?php _e( 'WordPress Resources', 'unlimited' ); ?></h3>
-			<p><?php _e( "Save time and money searching for WordPress products by following our recommendations.", "unlimited" ); ?></p>
-			<p>
-				<a target="_blank" class="button-primary"
-				   href="https://www.competethemes.com/wordpress-resources/"><?php _e( 'View Resources', 'unlimited' ); ?></a>
 			</p>
 		</div>
 		<div class="content content-review">
