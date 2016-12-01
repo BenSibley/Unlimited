fc1cb9564030d9b883c092059252c9bc6b1380eb
BenSibley <ben@competethemes.com>
2016-12-01 14:00:23 -0500

    options page styles now more reliable

--- theme-options.php
+++ theme-options.php
@@ -18,39 +18,43 @@ function unlimited_options_content() {
 	<div id="unlimited-dashboard-wrap" class="wrap">
 		<h2><?php _e( 'Unlimited Dashboard', 'unlimited' ); ?></h2>
 		<?php do_action( 'theme_options_before' ); ?>
-		<div class="content content-support">
-			<h3><?php _e( 'Get Started', 'unlimited' ); ?></h3>
-			<p><?php _e( "Not sure where to start? The <strong>Unlimited Getting Started Guide</strong> will take you step-by-step through every feature in Unlimited.", "unlimited" ); ?></p>
-			<p>
-				<a target="_blank" class="button-primary"
-				   href="https://www.competethemes.com/help/getting-started-unlimited/"><?php _e( 'View Guide', 'unlimited' ); ?></a>
-			</p>
-		</div>
-		<div class="content content-premium-upgrade">
-			<h3><?php _e( 'Unlimited Pro Plugin', 'unlimited' ); ?></h3>
-			<p><?php _e( 'Download the Unlimited Pro plugin and unlock custom colors, new layouts, background images, and more...', 'unlimited' ); ?></p>
-			<p>
-				<a target="_blank" class="button-primary"
-				   href="https://www.competethemes.com/unlimited-pro/"><?php _e( 'See Full Feature List', 'unlimited' ); ?></a>
-			</p>
-		</div>
-		<div class="content content-review">
-			<h3><?php _e( 'Leave a Review', 'unlimited' ); ?></h3>
-			<p><?php _e( 'Help others find unlimited by leaving a review on wordpress.org.', 'unlimited' ); ?></p>
-			<a target="_blank" class="button-primary" href="https://wordpress.org/support/view/theme-reviews/unlimited"><?php _e( 'Leave a Review', 'unlimited' ); ?></a>
-		</div>
-		<div class="content content-delete-settings">
-			<h3><?php _e( 'Reset Unlimited Settings', 'unlimited' ); ?></h3>
-			<p>
-				<?php printf( __( "<strong>Warning:</strong> Clicking this button will erase the Unlimited theme's current settings in the <a href='%s'>Customizer</a>.", 'unlimited' ), esc_url( $customizer_url ) ); ?>
-			</p>
-			<form method="post">
-				<input type="hidden" name="unlimited_reset_customizer" value="unlimited_reset_customizer_settings"/>
+		<div class="content-boxes">
+			<div class="content content-support">
+				<h3><?php _e( 'Get Started', 'unlimited' ); ?></h3>
+				<p><?php _e( "Not sure where to start? The <strong>Unlimited Getting Started Guide</strong> will take you step-by-step through every feature in Unlimited.", "unlimited" ); ?></p>
 				<p>
-					<?php wp_nonce_field( 'unlimited_reset_customizer_nonce', 'unlimited_reset_customizer_nonce' ); ?>
-					<?php submit_button( __( 'Reset Customizer Settings', 'unlimited' ), 'delete', 'delete', false ); ?>
+					<a target="_blank" class="button-primary"
+					   href="https://www.competethemes.com/help/getting-started-unlimited/"><?php _e( 'View Guide', 'unlimited' ); ?></a>
 				</p>
-			</form>
+			</div>
+			<?php if ( !function_exists( 'ct_unlimited_pro_init' ) ) : ?>
+				<div class="content content-premium-upgrade">
+					<h3><?php _e( 'Unlimited Pro Plugin', 'unlimited' ); ?></h3>
+					<p><?php _e( 'Download the Unlimited Pro plugin and unlock custom colors, new layouts, background images, and more...', 'unlimited' ); ?></p>
+					<p>
+						<a target="_blank" class="button-primary"
+						   href="https://www.competethemes.com/unlimited-pro/"><?php _e( 'See Full Feature List', 'unlimited' ); ?></a>
+					</p>
+				</div>
+			<?php endif; ?>
+			<div class="content content-review">
+				<h3><?php _e( 'Leave a Review', 'unlimited' ); ?></h3>
+				<p><?php _e( 'Help others find Unlimited by leaving a review on wordpress.org.', 'unlimited' ); ?></p>
+				<a target="_blank" class="button-primary" href="https://wordpress.org/support/view/theme-reviews/unlimited"><?php _e( 'Leave a Review', 'unlimited' ); ?></a>
+			</div>
+			<div class="content content-delete-settings">
+				<h3><?php _e( 'Reset Unlimited Settings', 'unlimited' ); ?></h3>
+				<p>
+					<?php printf( __( "<strong>Warning:</strong> Clicking this button will erase the Unlimited theme's current settings in the <a href='%s'>Customizer</a>.", 'unlimited' ), esc_url( $customizer_url ) ); ?>
+				</p>
+				<form method="post">
+					<input type="hidden" name="unlimited_reset_customizer" value="unlimited_reset_customizer_settings"/>
+					<p>
+						<?php wp_nonce_field( 'unlimited_reset_customizer_nonce', 'unlimited_reset_customizer_nonce' ); ?>
+						<?php submit_button( __( 'Reset Customizer Settings', 'unlimited' ), 'delete', 'delete', false ); ?>
+					</p>
+				</form>
+			</div>
 		</div>
 		<?php do_action( 'theme_options_after' ); ?>
 	</div>
