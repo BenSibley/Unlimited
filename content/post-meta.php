e30e522734b2743ab8d30a9e768e97c9ef937f83
BenSibley <ben@competethemes.com>
2017-02-03 11:11:23 -0500

    added aria-hidden to ornamental icons

--- content/post-meta.php
+++ content/post-meta.php
@@ -2,20 +2,20 @@
 	<div class="date-meta">
 		<a href="<?php echo esc_url( get_month_link( get_the_date( 'Y' ), get_the_date( 'n' ) ) ); ?>"
 		   title="Posts from <?php echo get_the_date( 'F' ); ?>">
-			<i class="fa fa-calendar"></i>
+			<i class="fa fa-calendar" aria-hidden="true"></i>
 			<span><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) ); ?></span>
 		</a>
 	</div>
 	<div class="author-meta">
 		<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
 		   title="Posts by <?php the_author(); ?>">
-			<i class="fa fa-user"></i>
+			<i class="fa fa-user" aria-hidden="true"></i>
 			<span><?php the_author(); ?></span>
 		</a>
 	</div>
 	<div class="comments-meta">
 		<a href="<?php echo esc_url( get_comments_link() ); ?>" title="Comments for this post">
-			<i class="fa fa-comment"></i>
+			<i class="fa fa-comment" aria-hidden="true"></i>
 			<span>
 				<?php
 				if ( ! comments_open() && get_comments_number() < 1 ) :
