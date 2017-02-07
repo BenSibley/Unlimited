a0d9ab50156886d7be0fbc3f1a97b1ad67b3b17b
BenSibley <ben@competethemes.com>
2017-02-07 11:19:40 -0500

    additional disambiguation

--- content/post-categories.php
+++ content/post-categories.php
@@ -2,15 +2,15 @@
 
 $categories = get_the_category( $post->ID );
 $separator  = ', ';
-$output     = __( 'Posted in', 'unlimited' ) . ' ';
+$output     = _x( 'Posted in', 'posted in category', 'unlimited' ) . ' ';
 
 if ( $categories ) {
 	echo '<p class="post-categories">';
 	foreach ( $categories as $category ) {
 		if ( $category === end( $categories ) && $category !== reset( $categories ) ) {
-			$output .= __( 'and', 'unlimited' ) . ' ';
+			$output .= _x( 'and', 'category 1, category 2, AND category 3', 'unlimited' ) . ' ';
 		}
-		$output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="' . esc_attr( sprintf( __( "View all posts in %s", 'unlimited' ), $category->name ) ) . '">' . esc_html( $category->cat_name ) . '</a>' . $separator;
+		$output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="' . esc_attr( sprintf( _x( "View all posts in %s", 'View all posts in category', 'unlimited' ), $category->name ) ) . '">' . esc_html( $category->cat_name ) . '</a>' . $separator;
 	}
 	echo trim( $output, $separator );
 	echo "</p>";
