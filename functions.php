5ca2c6afcdb41b703e487b663668348ebde8217a
BenSibley <ben@competethemes.com>
2017-08-11 10:36:17 -0400

    added review request

--- functions.php
+++ functions.php
@@ -4,6 +4,15 @@ require_once( trailingslashit( get_template_directory() ) . 'theme-options.php' 
 foreach ( glob( trailingslashit( get_template_directory() ) . 'inc/*' ) as $filename ) {
 	include $filename;
 }
+require_once( trailingslashit( get_template_directory() ) . 'dnh/handler.php' );
+new WP_Review_Me( array(
+		'days_after' => 14,
+		'type'       => 'theme',
+		'slug'       => 'unlimited',
+		'message'    => __( 'Hey! Sorry to interrupt, but you\'ve been using Unlimited for a little while now. If you\'re happy with this theme, could you take a minute to leave a review? <i>You won\'t see this notice again after closing it.</i>', 'unlimited' )
+	)
+);
+
 
 if ( ! function_exists( ( 'ct_unlimited_set_content_width' ) ) ) {
 	function ct_unlimited_set_content_width() {
