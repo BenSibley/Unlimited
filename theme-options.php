50aa4b52c285d248b2df0562d20b71966b239887
BenSibley <ben@competethemes.com>
2018-11-03 14:02:02 -0400

    re-designed Unlimited Dashboard

--- theme-options.php
+++ theme-options.php
@@ -1,11 +1,11 @@
 <?php
 
-function unlimited_register_theme_page() {
-	add_theme_page( sprintf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme( get_template() ) ), sprintf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme( get_template() ) ), 'edit_theme_options', 'unlimited-options', 'unlimited_options_content', 'unlimited_options_content' );
+function ct_unlimited_register_theme_page() {
+	add_theme_page( sprintf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme() ), sprintf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme() ), 'edit_theme_options', 'unlimited-options', 'ct_unlimited_options_content', 'ct_unlimited_options_content' );
 }
-add_action( 'admin_menu', 'unlimited_register_theme_page' );
+add_action( 'admin_menu', 'ct_unlimited_register_theme_page' );
 
-function unlimited_options_content() {
+function ct_unlimited_options_content() {
 
 	$customizer_url = add_query_arg(
 		array(
@@ -14,49 +14,203 @@ function unlimited_options_content() {
 		),
 		admin_url( 'customize.php' )
 	);
+	$pro_url = 'https://www.competethemes.com/unlimited-pro/?utm_source=wp-dashboard&utm_medium=Dashboard&utm_campaign=Unlimited%20Pro%20-%20Dashboard';
 	?>
-	<div id="unlimited-dashboard-wrap" class="wrap">
-		<h2><?php printf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme( get_template() ) ); ?></h2>
+	<div id="unlimited-dashboard-wrap" class="wrap unlimited-dashboard-wrap">
+		<h2><?php printf( esc_html__( '%s Dashboard', 'unlimited' ), wp_get_theme() ); ?></h2>
 		<?php do_action( 'theme_options_before' ); ?>
-		<div class="content-boxes">
-			<div class="content content-support">
-				<h3><?php esc_html_e( 'Get Started', 'unlimited' ); ?></h3>
-				<p><?php printf( __( 'Not sure where to start? The <strong>%1$s Getting Started Guide</strong> will take you step-by-step through every feature in %1$s.', 'unlimited' ), wp_get_theme( get_template() ) ); ?></p>
-				<p>
-					<a target="_blank" class="button-primary"
-					   href="https://www.competethemes.com/help/getting-started-unlimited/"><?php esc_html_e( 'View Guide', 'unlimited' ); ?></a>
-				</p>
+		<div class="main">
+			<?php if ( function_exists( 'ct_unlimited_pro_init' ) ) : ?>
+			<div class="thanks-upgrading" style="background-image: url(<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/bg-texture.png'; ?>)">
+				<h3>Thanks for upgrading!</h3>
+				<p>You can find the new features in the Customizer</p>
 			</div>
-			<?php if ( !function_exists( 'ct_unlimited_pro_init' ) ) : ?>
-				<div class="content content-premium-upgrade">
-					<h3><?php printf( esc_html__( '%s Pro Plugin', 'unlimited' ), wp_get_theme( get_template() ) ); ?></h3>
-					<p><?php printf( esc_html__( 'Download the %s Pro plugin and unlock custom colors, new layouts, background images, and more...', 'unlimited' ), wp_get_theme( get_template() ) ); ?></p>
-					<p>
-						<a target="_blank" class="button-primary"
-						   href="https://www.competethemes.com/unlimited-pro/"><?php esc_html_e( 'See Full Feature List', 'unlimited' ); ?></a>
-					</p>
-				</div>
 			<?php endif; ?>
-			<div class="content content-review">
-				<h3><?php esc_html_e( 'Leave a Review', 'unlimited' ); ?></h3>
-				<p><?php printf( esc_html__( 'Help others find %s by leaving a review on wordpress.org.', 'unlimited' ), wp_get_theme( get_template() ) ); ?></p>
-				<a target="_blank" class="button-primary" href="https://wordpress.org/support/theme/unlimited/reviews/"><?php esc_html_e( 'Leave a Review', 'unlimited' ); ?></a>
+			<?php if ( !function_exists( 'ct_unlimited_pro_init' ) ) : ?>
+			<div class="getting-started">
+				<h3>Get Started with Unlimited</h3>
+				<p>Follow this step-by-step guide to customize your website with Unlimited:</p>
+				<a href="https://www.competethemes.com/help/getting-started-unlimited/" target="_blank">Read the Getting Started Guide</a>
 			</div>
-			<div class="content content-presspad">
-				<h3><?php esc_html_e( 'Turn Unlimited into a Mobile App', 'unlimited' ); ?></h3>
-				<p><?php printf( esc_html__( '%s can be converted into a mobile app and listed on the App Store and Google Play Store with the help of PressPad News. Read our tutorial to learn more.', 'unlimited' ), wp_get_theme( get_template() ) ); ?></p>
-				<a target="_blank" class="button-primary" href="https://www.competethemes.com/help/convert-mobile-app-unlimited/"><?php esc_html_e( 'Read Tutorial', 'unlimited' ); ?></a>
+			<div class="pro">
+				<h3>Customize More with Unlimited Pro</h3>
+				<p>Add 14 new customization features to your site with the <a href="<?php echo $pro_url; ?>" target="_blank">Unlimited Pro</a> plugin.</p>
+				<ul class="feature-list">
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/layouts.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>New Layouts</h4>
+							<p>New layouts help your content look and perform its best. You can switch to new layouts effortlessly from the Customizer, or from specific posts or pages.</p>
+							<p>Unlimited Pro adds 6 new layouts.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/custom-colors.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Custom Colors</h4>
+							<p>Custom colors let you match the color of your site with your brand. Point-and-click to select a color, and watch your site update instantly.</p>
+							<p>With 77 color controls, you can change the color of any element on your site.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/fonts.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>New Fonts</h4>
+							<p>Stylish new fonts add character and charm to your content. Select and instantly preview fonts from the Customizer.</p>
+							<p>Since Unlimited Pro is powered by Google Fonts, it comes with 728 fonts for you to choose from.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/header-image.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Flexible Header Image</h4>
+							<p>Header images welcome visitors and set your site apart. Upload your image and quickly resize it to the perfect size.</p>
+							<p>Display the header image on just the homepage, or leave it sitewide and link it to the homepage.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/featured-videos.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Featured Videos</h4>
+							<p>Featured Videos are an easy way to share videos in place of Featured Images. Instantly embed a Youtube video by copying and pasting its URL into an input.</p>
+							<p>Unlimited Pro auto-embeds videos from Youtube, Vimeo, DailyMotion, Flickr, Animoto, TED, Blip, Cloudup, FunnyOrDie, Hulu, Vine, WordPress.tv, and VideoPress.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/featured-sliders.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Featured Sliders</h4>
+							<p>Featured Sliders are an easy way to share image sliders in place of Featured Images. Quickly add responsive sliders to any page or post.</p>
+							<p>Unlimited Pro integrates with the free Meta Slider plugin with styling and sizing controls for your sliders.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/background-images.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Background Images</h4>
+							<p>Background images help you stand out from the rest. Upload a unique image to use as the backdrop for your site.</p>
+							<p>Background images are automatically centered and sized to fit the screen.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/background-textures.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Background Textures</h4>
+							<p>Background textures transform the look and feel of your site. Switch to a textured background with a click.</p>
+							<p>Unlimited Pro includes 39 bundled textures to choose from.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/featured-image-size.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Featured Image Size</h4>
+							<p>Set each Featured Image to the perfect size. You can change the aspect ratio for all Featured Images and individual Featured Images with ease.</p>
+							<p>Unlimited Pro includes twelve different aspect ratios for your Featured Images.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/widget-areas.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>New Widget Areas</h4>
+							<p>Utilize a sidebar and four additional widget areas for greater flexibility. Increase ad revenue and generate more email subscribers by adding widgets throughout your site.</p>
+							<p>Unlimited Pro adds 5 new widget areas.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/menus.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Additional Menus</h4>
+							<p>Additional menus allow you to expand and optimize your site's navigation. Quickly create and publish new menus just like the Primary menu.</p>
+							<p>Unlimited Pro adds a Secondary and Footer menu to Unlimited.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/fixed-menus.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Navigational Styles</h4>
+							<p>Fixed position menus help visitors discover your content by keeping the navigation present at all times. Easily switch between menu styles from the Customizer.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/display-controls.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Display Controls</h4>
+							<p>Display controls let you display the parts of your site you want to show off, and hide the rest. Remove elements with just one click.</p>
+							<p>Unlimited Pro includes display controls for 11 different elements.</p>
+						</div>
+					</li>
+					<li>
+						<div class="image">
+							<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/footer-text.png'; ?>" />
+						</div>
+						<div class="text">
+							<h4>Custom Footer Text</h4>
+							<p>Custom footer text lets you further brand your site. Just start typing to add your own text to the footer.</p>
+							<p>The footer text supports plain text and full HTML for adding links.</p>
+						</div>
+					</li>
+				</ul>
+				<p><a href="<?php echo $pro_url; ?>" target="_blank">Click here</a> to view Unlimited Pro now, and see what it can do for your site.</p>
 			</div>
-			<div class="content content-delete-settings">
-				<h3><?php printf( esc_html__( 'Reset %s Settings', 'unlimited' ), wp_get_theme( get_template() ) ); ?></h3>
-				<p>
-					<?php printf( __( '<strong>Warning:</strong> Clicking this button will erase the %2$s theme\'s current settings in the <a href="%1$s">Customizer</a>.', 'unlimited' ), esc_url( $customizer_url ), wp_get_theme( get_template() ) ); ?>
-				</p>
+			<div class="pro-ad" style="background-image: url(<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/bg-texture.png'; ?>)">
+				<h3>Add Incredible Flexibility to Your Site</h3>
+				<p>Start customizing with Unlimited Pro today</p>
+				<a href="<?php echo $pro_url; ?>" target="_blank">View Unlimited Pro</a>
+			</div>
+			<?php endif; ?>
+		</div>
+		<div class="sidebar">
+			<div class="dashboard-widget">
+				<h4>More Amazing Resources</h4>
+				<ul>
+					<li><a href="https://www.competethemes.com/documentation/unlimited-support-center/" target="_blank">Unlimited Support Center</a></li>
+					<li><a href="https://wordpress.org/support/theme/unlimited" target="_blank">Support Forum</a></li>
+					<li><a href="https://www.competethemes.com/help/unlimited-changelog/" target="_blank">Changelog</a></li>
+					<li><a href="https://www.competethemes.com/help/unlimited-css-snippets/" target="_blank">CSS Snippets</a></li>
+					<li><a href="https://www.competethemes.com/help/child-theme-unlimited/" target="_blank">Starter child theme</a></li>
+					<li><a href="https://www.competethemes.com/help/unlimited-demo-data/" target="_blank">Unlimited demo data</a></li>
+					<li><a href="<?php echo $pro_url; ?>" target="_blank">Unlimited Pro</a></li>
+				</ul>
+			</div>
+			<div class="dashboard-widget">
+				<h4>User Reviews</h4>
+				<img src="<?php echo trailingslashit(get_template_directory_uri()) . 'assets/images/reviews.png'; ?>" />
+				<p>Users are loving Unlimited! <a href="https://wordpress.org/support/theme/unlimited/reviews/?filter=5#new-post" target="_blank">Click here</a> to leave your own review</p>
+			</div>
+			<div class="dashboard-widget">
+				<h4>Reset Customizer Settings</h4>
+				<p><b>Warning:</b> Clicking this buttin will erase the Unlimited theme's current settings in the Customizer.</p>
 				<form method="post">
 					<input type="hidden" name="unlimited_reset_customizer" value="unlimited_reset_customizer_settings"/>
 					<p>
 						<?php wp_nonce_field( 'unlimited_reset_customizer_nonce', 'unlimited_reset_customizer_nonce' ); ?>
-						<?php submit_button( esc_html__( 'Reset Customizer Settings', 'unlimited' ), 'delete', 'delete', false ); ?>
+						<?php submit_button( 'Reset Customizer Settings', 'delete', 'delete', false ); ?>
 					</p>
 				</form>
 			</div>
