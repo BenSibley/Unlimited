0f8745755f9b4c604c2ec264eb6072d1f8ee8adc
BenSibley <ben@competethemes.com>
2019-02-26 10:16:38 -0500

    missing i18n

--- content/post-meta.php
+++ content/post-meta.php
@@ -1,20 +1,20 @@
 <div class="post-meta">
 	<div class="date-meta">
 		<a href="<?php echo esc_url( get_month_link( get_the_date( 'Y' ), get_the_date( 'n' ) ) ); ?>"
-		   title="Posts from <?php echo get_the_date( 'F' ); ?>">
+		   title="<?php echo esc_html__( 'Posts from', 'unlimited' ); ?> <?php echo get_the_date( 'F' ); ?>">
 			<i class="fas fa-calendar" aria-hidden="true"></i>
 			<span><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'c' ) ) ); ?></span>
 		</a>
 	</div>
 	<div class="author-meta">
 		<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
-		   title="Posts by <?php the_author(); ?>">
+		   title="<?php echo esc_html__( 'Posts by', 'unlimited' ); ?> <?php the_author(); ?>">
 			<i class="fas fa-user" aria-hidden="true"></i>
 			<span><?php the_author(); ?></span>
 		</a>
 	</div>
 	<div class="comments-meta">
-		<a href="<?php echo esc_url( get_comments_link() ); ?>" title="Comments for this post">
+		<a href="<?php echo esc_url( get_comments_link() ); ?>" title="<?php echo esc_html__( 'Comments for this post', 'unlimited' ); ?>">
 			<i class="fas fa-comment" aria-hidden="true"></i>
 			<span>
 				<?php
