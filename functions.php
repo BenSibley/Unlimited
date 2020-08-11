0b48605f6252e57d2402fef6fc4bd925757ce1cf
BenSibley <ben@competethemes.com>
2020-08-11 12:10:39 -0400

    removed incorrect theme support call

--- functions.php
+++ functions.php
@@ -60,7 +60,6 @@ if ( ! function_exists( 'unlimited_theme_setup' ) ) {
 
 		// Gutenberg - wide & full images
 		add_theme_support( 'align-wide' );
-		add_theme_support( 'align-full' );
 
 		// Gutenberg - add support for editor styles
 		add_theme_support('editor-styles');
