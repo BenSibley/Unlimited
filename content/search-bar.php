d9c04803d5f355c4ff249f1135c3904937324f9b
Ben Sibley <ben@competethemes.com>
2020-12-23 13:46:44 -0500

    removed extra screen reader text

--- content/search-bar.php
+++ content/search-bar.php
@@ -4,7 +4,6 @@
 	</button>
 	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<label class="screen-reader-text" for="search-field"><?php esc_html_e( 'Search', 'unlimited' ); ?></label>
-		<label class="screen-reader-text"><?php echo esc_html_x( 'Search', 'verb', 'unlimited' ); ?></label>
 		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search...', 'unlimited' ); ?>" value=""
 		       name="s" title="<?php esc_attr_e( 'Search for:', 'unlimited' ); ?>" tabindex="-1"/>
 	</form>
