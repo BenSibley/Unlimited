793870b5649a8a7821eb572ded2df2fd644dd9d1
BenSibley <ben@competethemes.com>
2024-11-12 11:24:15 -0500

    Mistake

--- content/post-meta.php
+++ content/post-meta.php
@@ -8,7 +8,7 @@
 	</div>
 	<div class="author-meta">
 		<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
-		   title="<?php echo esc_attr__( 'Posts by', 'unlimited' ); ?> <?php echo esc_html(get_the_author()); ?>">
+		   title="<?php echo esc_attr__( 'Posts by', 'unlimited' ); ?> <?php echo esc_attr(get_the_author()); ?>">
 			<i class="fas fa-user" aria-hidden="true"></i>
 			<span><?php echo esc_html(get_the_author()); ?></span>
 		</a>
