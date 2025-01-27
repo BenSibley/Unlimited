ae42b54c3dc151ae7201818f71dd1bfcd75ef4c0
BenSibley <ben@competethemes.com>
2025-01-27 11:02:57 -0500

    Properly associated label with input

--- content/search-bar.php
+++ content/search-bar.php
@@ -4,8 +4,8 @@
 		<i class="fas fa-search"></i>
 	</button>
 	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<label class="screen-reader-text" for="search-field"><?php esc_html_e( 'Search', 'unlimited' ); ?></label>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search...', 'unlimited' ); ?>" value=""
+		<label class="screen-reader-text" for="header-search-field"><?php esc_html_e( 'Search', 'unlimited' ); ?></label>
+		<input id="header-search-field" name="header-search-field" class="search-field" type="search"  placeholder="<?php esc_attr_e( 'Search...', 'unlimited' ); ?>" value=""
 		       name="s" title="<?php esc_attr_e( 'Search for:', 'unlimited' ); ?>" tabindex="-1"/>
 	</form>
 </div>
